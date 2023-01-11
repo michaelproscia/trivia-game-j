987b1aaf71b910cb78348398bfaeacc3c1cf80e2
Michael Proscia <macp612@gmail.com>
2023-01-10 20:04:17 -0500

    clues logic and style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CategoryColumn from "./components/CategoryColumn/CategoryColumn";
+import { nanoid } from "nanoid";
 
 function App() {
+  //Set state for data
   const [data, setData] = useState([]);
-  const [title, setTitle] = useState("");
+  const [isGameStarted, setIsGameStarted] = useState(false);
 
-  function handleCall() {
-    fetch(
-      `https://jservice.io/api/categories?count=6&offset=${Math.floor(
-        Math.random() * 3000
-      )}`
-    )
-      .then((res) => res.json())
-      .then((data) => setData(data))
-      .then(() => setTitle(data[0].title));
+  //Retrieving random category from jService
+  useEffect(() => {
+    for (let i = 0; i < 6; i++) {
+      fetch(
+        `https:jservice.io/api/category?id=${Math.floor(Math.random() * 2000)}`
+      )
+        .then((res) => res.json())
+        .then((data) => setData((prevData) => [...prevData, data]));
+    }
+  }, []);
+
+  function handleGameStart() {
+    setIsGameStarted(true);
   }
 
+  const columnsArr = data.map((col, i) => {
+    if (i < 6) {
+      return <CategoryColumn key={nanoid()} data={col} />;
+    }
+  });
+
   return (
-    <div>
-      <button onClick={handleCall}>Hello</button>
-      <CategoryColumn data={data} title={title} />
+    <div className="game-board">
+      {!isGameStarted && (
+        <button className="start-button" onClick={handleGameStart}>
+          Start
+        </button>
+      )}
+      {isGameStarted && columnsArr}
     </div>
   );
 }
--- src/components/CategoryColumn/CategoryColumn.js
+++ src/components/CategoryColumn/CategoryColumn.js
@@ -1,12 +1,37 @@
+import "./CategoryColumn.css";
+import { useState } from "react";
+
 export default function CategoryColumn(props) {
+  function handleClick(clue) {
+    clue.isClicked = !clue.isClicked;
+    console.log(clue);
+  }
+  props.data.clues.length = 5;
+
+  const clueysArr = props.data.clues.map((clue) => ({
+    ...clue,
+    isClicked: false,
+  }));
+  const cluesArr = clueysArr.map((clue, i) => {
+    if (!clue.isClicked) {
+      return (
+        <div className="question-box" onClick={() => handleClick(clue)}>
+          {i === 0 ? 100 : i * 100 + 100}
+        </div>
+      );
+    } else {
+      return (
+        <div className="question-box" onClick={() => handleClick(clue)}>
+          {clue.question}
+        </div>
+      );
+    }
+  });
+
   return (
     <div>
-      <div>{props.title}</div>
-      <div data-key=></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className="category-title">{props.data.title.toUpperCase()}</div>
+      <div className="question-box">{cluesArr}</div>
     </div>
   );
 }
